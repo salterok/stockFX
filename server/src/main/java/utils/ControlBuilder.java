780b127afad5a8ea16702da382b8e30fdcf72e89
salterok <salterok@gmail.com>
2015-05-28 19:44:28 +0300

    add localization support

--- server/src/main/java/utils/ControlBuilder.java
+++ server/src/main/java/utils/ControlBuilder.java
@@ -3,13 +3,19 @@ package utils;
 import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Created by salterok on 09.05.2015.
  */
 public class ControlBuilder {
+    public static Locale locale = Locale.forLanguageTag("ru");
+
     private static void _build(Object instance, String path) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(instance.getClass().getResource(path));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles." + instance.getClass().getSimpleName(), locale);
+        fxmlLoader.setResources(bundle);
         fxmlLoader.setRoot(instance);
         fxmlLoader.setController(instance);
         fxmlLoader.load();
